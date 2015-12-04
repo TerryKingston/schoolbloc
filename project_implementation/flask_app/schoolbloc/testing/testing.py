9e9cd3decd3117bf66f45da773263119b3598e98
Landon GB <landogbland@gmail.com>
2015-12-03 23:15:48 -0700

    Better unit test examples

--- project_implementation/flask_app/schoolbloc/testing/testing.py
+++ project_implementation/flask_app/schoolbloc/testing/testing.py
@@ -1,6 +1,5 @@
 import unittest
 from flask import json
-from sqlalchemy.exc import IntegrityError
 from schoolbloc import db, app, User
 from schoolbloc.users.models import Role
 
@@ -19,18 +18,25 @@ class BaseTestClass(unittest.TestCase):
         with app.app_context():
             db.drop_all()
 
+    def parse_response_json(self, response):
+        # God knows why, but for some reason, sometimes, the first json.loads
+        # returns a string instead of a dict/list/whatever. This is a dumb fix
+        data = json.loads(response.get_data(as_text=True))
+        if isinstance(data, str):
+            data = json.loads(data)
+        return data
+
     def login(self, username, password):
         login_data = {'username': username, 'password': password}
         response = self.app.post('/auth', data=json.dumps(login_data),
                                  content_type='application/json')
-        data = json.loads(response.get_data(as_text=True))
-        return data['access_token']
+        data = self.parse_response_json(response)
+        try:
+            return data['access_token'], response.status_code
+        except KeyError:
+            return data, response.status_code
 
     def _create_roles(self):
         for role in ('admin', 'teacher', 'student'):
             db.session.add(Role(role_type=role))
 
-    def _create_users(self):
-        db.session.add(User('student', 'student', 'student'))
-        db.session.add(User('teacher', 'teacher', 'teacher'))
-        db.session.add(User('admin', 'admin', 'admin'))
--- project_implementation/flask_app/schoolbloc/users/tests.py
+++ project_implementation/flask_app/schoolbloc/users/tests.py
@@ -1,15 +1,40 @@
 import unittest
-from schoolbloc import db
+from schoolbloc import User, db
 from schoolbloc.testing.testing import BaseTestClass
-from schoolbloc.users.models import User
 
 
 class UserTests(BaseTestClass):
     """ Unit tests for Roles and Users """
 
-    def test_login(self):
-        db.session.add(User('student', 'student', 'student'))
-        self.login('student', 'student')
+    def test_valid_login(self):
+        db.session.add(User(username='student', password='student', role_type='student'))
+        key, status_code = self.login('student', 'student')
+        self.assertEqual(status_code, 200)
+
+    def test_invalid_login(self):
+        db.session.add(User(username='student', password='student', role_type='student'))
+        key, status_code = self.login('student', 'bad_pw')
+        self.assertEqual(status_code, 401)
+
+    def test_get_user(self):
+        # setup users
+        u1 = User(username='student', password='student', role_type='student')
+        db.session.add(u1)
+        db.session.commit()
+
+        # Get jwt auth key
+        key, _ = self.login('student', 'student')
+
+        # Perform restful get
+        headers = {'Authorization': 'JWT {}'.format(key)}
+        response = self.app.get('/api/users/{}'.format(u1.id), headers=headers,
+                                content_type='application/json')
+        data = self.parse_response_json(response)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['username'], u1.username)
+
+        # TODO students can get only themselves, admins and teachers can get
+        #      anyone
 
 if __name__ == '__main__':
     unittest.main()
