680a16b5e81e91378e0fb40cec7e3b05a0dbdc74
Landon GB <landogbland@gmail.com>
2016-01-11 16:49:27 -0700

    Serializer seems to only work on models side. That's alright

--- project_implementation/flask_app/schoolbloc/constraints/views.py
+++ project_implementation/flask_app/schoolbloc/constraints/views.py
@@ -9,25 +9,12 @@ mod = Blueprint('api', __name__)
 api = Api(mod)
 
 
-class Serializer:
-    """
-    Provides a base model for our database tables and constraints. This
-    provides a serialize method that will be utilized by our rest endpoints
-    """
-    def serialize(self):
-        results = {}
-        columns = self.__table__.columns.values()
-        for column in columns:
-            results[column.name] = getattr(self, column.name)
-        return results
-
-
 def register_rest_orm(orm):
     name = orm.__tablename__
-    cls = type(name, (TestRest, Serializer), {'orm': orm})
+    cls = type(name, (TestRest,), {'orm': orm})
     api.add_resource(cls, '/api/{}/<int:orm_id>'.format(name))
 
-    cls_list = type(name + 'list', (TestRestList, Serializer), {'orm': orm})
+    cls_list = type(name + 'list', (TestRestList,), {'orm': orm})
     api.add_resource(cls_list, '/api/{}'.format(name))
 
 register_rest_orm(Test1)
