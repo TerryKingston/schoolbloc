925e9083095f5e77923dc3c1fa5a2abb0cac70db
Landon GB <landogbland@gmail.com>
2016-01-11 17:11:52 -0700

    Scary magic. We will see how this works

--- project_implementation/flask_app/schoolbloc/constraints/views.py
+++ project_implementation/flask_app/schoolbloc/constraints/views.py
@@ -2,8 +2,11 @@ from flask import Blueprint
 from flask.ext.restful import Api
 from schoolbloc.constraints.restthing import TestRest, TestRestList
 
-from schoolbloc.constraints.models import Test1, Test2
-
+from schoolbloc.constraints.models import CoursesStudent, Course, \
+    Teacher, Subject, StudentsStudentGroup, Student, StudentGroup, \
+    ScheduledClassesStudent, ScheduledClass, Schedule, ClassroomsCourse, \
+    ClassroomsTeacher, Classroom, CoursesSubject, CoursesStudentGroup, \
+    CoursesTeacher
 
 mod = Blueprint('api', __name__)
 api = Api(mod)
@@ -17,5 +20,19 @@ def register_rest_orm(orm):
     cls_list = type(name + 'list', (TestRestList,), {'orm': orm})
     api.add_resource(cls_list, '/api/{}'.format(name))
 
-register_rest_orm(Test1)
-register_rest_orm(Test2)
+register_rest_orm(Course)
+register_rest_orm(CoursesStudent)
+register_rest_orm(CoursesTeacher)
+register_rest_orm(CoursesStudentGroup)
+register_rest_orm(CoursesSubject)
+register_rest_orm(Classroom)
+register_rest_orm(ClassroomsTeacher)
+register_rest_orm(ClassroomsCourse)
+register_rest_orm(Schedule)
+register_rest_orm(ScheduledClass)
+register_rest_orm(ScheduledClassesStudent)
+register_rest_orm(StudentGroup)
+register_rest_orm(Student)
+register_rest_orm(StudentsStudentGroup)
+register_rest_orm(Subject)
+register_rest_orm(Teacher)
