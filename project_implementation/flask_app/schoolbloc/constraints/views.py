43b0a2894be229a50086538d56e42e762cd28358
Landon GB <landogbland@gmail.com>
2016-01-11 22:58:50 -0700

    Add access control functions to dynamically created classes

--- project_implementation/flask_app/schoolbloc/constraints/views.py
+++ project_implementation/flask_app/schoolbloc/constraints/views.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 from flask.ext.restful import Api
+
+from schoolbloc import auth_required
 from schoolbloc.constraints.restthing import TestRest, TestRestList
 
 from schoolbloc.constraints.models import CoursesStudent, Course, \
@@ -12,15 +14,54 @@ mod = Blueprint('api', __name__)
 api = Api(mod)
 
 
-def register_rest_orm(orm):
-    name = orm.__tablename__
-    cls = type(name, (TestRest,), {'orm': orm})
-    api.add_resource(cls, '/api/{}/<int:orm_id>'.format(name))
+def register_rest_orm(orm, get='admin', put='admin', post='admin',
+                      delete='admin', list='admin'):
+    """
+    Registar a sqlalchemy orm model to be exposed via the rest api. By default,
+    only admins will be authorized to access each of the rest methods. You can override
+    this by passing in a value to get, put, post, delete, and list to add different
+    access controls to each individual method.
 
+    The valid values you can pass to these are:
+        - A string, which is the one role who can access this endpoint
+        - A list, which is a list of roles (strings) who can access this endpoint
+        - None, which is anyone with a valid JWT (login token) can access this endpoint
+        - False, which is anyone can access this endpoint
+
+    This returns the classes created for this rest endpoint (class for interacting
+    with a specific object, and class_list for listing all objects and creating
+    new ones), which lets you override specific functions if so desired.
+    IE, if you want to use this, but want to override the POST method, you can
+    call this method, then override the post method with cls.post = <a_different_method>.
+    """
+    # We are basing the endpoint name on the tablename for simplicity sake
+    name = orm.__tablename__
+
+    # Create the single object get/modify/delete endpoints, and apply access controls
+    cls = type(name, (TestRest,), {'orm': orm})
+    if get is not False:
+        cls.get = auth_required(roles=get)(cls.get)
+    if put is not False:
+        cls.put = auth_required(roles=put)(cls.put)
+    if delete is not False:
+        cls.delete = auth_required(roles=delete)(cls.delete)
+
+    # Create list/post endpoint, and apply access control decorators if present
     cls_list = type(name + 'list', (TestRestList,), {'orm': orm})
+    if list is not False:
+        cls_list.get = auth_required(roles=list)(cls_list.get)
+    if post is not False:
+        cls_list.post = auth_required(roles=post)(cls_list.post)
+
+    # Register the classes to the rest api
+    api.add_resource(cls, '/api/{}/<int:orm_id>'.format(name))
     api.add_resource(cls_list, '/api/{}'.format(name))
 
-register_rest_orm(Course)
+    # Return the classes, so that the caller can overwrite a function if desired
+    return cls, cls_list
+
+
+register_rest_orm(Course, get=['teacher', 'admin'], list=['teacher', 'admin'])
 register_rest_orm(CoursesStudent)
 register_rest_orm(CoursesTeacher)
 register_rest_orm(CoursesStudentGroup)
