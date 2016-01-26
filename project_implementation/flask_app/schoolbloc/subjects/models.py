edebe9dc11dac59ec81655cb950f02279083fc73
Terry Kingston <tjkingston@gmail.com>
2016-01-25 20:29:13 -0700

    Added SubjectsStudentGroup class to assign a subject to a student group

--- project_implementation/flask_app/schoolbloc/subjects/models.py
+++ project_implementation/flask_app/schoolbloc/subjects/models.py
@@ -19,3 +19,15 @@ class Subject(db.Model):
             'id': self.id,
             'name': self.name,
         }
+
+class SubjectsStudentGroup(db.Model):
+    """
+    ORM object for mapping from Subject to Student Group
+    """
+    __tablename__ = 'subjects_student_groups'
+    id = db.Column(db.Integer, primary_key=True)
+    subject_id = db.Column(db.Integer, db.ForeignKey('subjects.id'), nullable=False)
+    student_group_id = db.Column(db.Integer, db.ForeignKey('student_groups.id'), nullable=False)
+    active = db.Column(db.Boolean, nullable=False, default=True)
+    subject = db.relationship("Subject", backref="subjects_student_groups")
+    course = db.relationship("StudentGroup", backref="subjects_student_groups")
