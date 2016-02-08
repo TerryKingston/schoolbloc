c7863f4b7671f7471ec1b977ce391ffbdb71cba1
Ryan Saunders <rhyeen@Ryans-MacBook-Pro.local>
2016-02-08 16:07:18 -0700

    Linked to back end (still have a few empty facts, such as "subjects")

--- project_implementation/angular_app/app/scripts/services/table-entries.service.js
+++ project_implementation/angular_app/app/scripts/services/table-entries.service.js
@@ -3,7 +3,17 @@
 /**
  * Provides data objects for large tables
  */
-angular.module('sbAngularApp').factory('tableEntriesService', [ function() {
+angular.module('sbAngularApp').factory('tableEntriesService', ['$q', '$http', 'commonService', function($q, $http, commonService) {
+	var SERVER_ROOT = "api/",
+		CLASSROOM_URL = SERVER_ROOT + "classrooms",
+		COURSE_URL = SERVER_ROOT + "courses",
+		STUDENT_URL = SERVER_ROOT + "students",
+		STUDENT_GROUP_URL = SERVER_ROOT + "student_groups",
+		SUBJECT_URL = SERVER_ROOT + "subjects",
+		TEACHER_URL = SERVER_ROOT + "teachers",
+		TIME_URL = SERVER_ROOT + "times";
+		
+
 	// never reinstantiate: reference will be lost with bound controllers
 	var tableConfig = {
 		tableType: null,
@@ -480,55 +490,114 @@ angular.module('sbAngularApp').factory('tableEntriesService', [ function() {
 
 			for (i = 0; i < ftc.length; i++) {
 				if (ftc[i].type === "constraint") {
-					ftc[i].facts = self.getConstraintFacts(ftc[i].key);
+					self.getConstraintFacts(ftc[i].key, i).then(function (data) {
+						ftc[data.index].facts = data.data;
+					}, function (data) {
+						// @TODO: display error message
+						// where data.data === message
+					});
 				}
 			}
 		},
 
-		getConstraintFacts: function(constraintName) {
+		/**
+		 * Retrieves the array of facts for a given constraint.
+		 * index is simply based back through return deferrment as a way to keep track of an index in a for loop
+		 */
+		getConstraintFacts: function(constraintName, index) {
+			var url;
+			var deferred = $q.defer();
+
 			if (!constraintName) {
 				return null;
 			}
 
 			// @TODO: change to get from back-end instead
 			if (constraintName === "time") {
-				return [
-					{
-						start: '1215',
-						end: '1300'
-					},
-					{
-						start: '1420',
-						end: '1520'
-					},
-					{
-						start: '1400',
-						end: '1445'
-					}
-				];
+				deferred.resolve({
+					index: index,
+					data: [
+						"12:15PM-01:00PM",
+						"12:20PM-14:20PM",
+						"2:00PM-2:45PM"
+					]
+				});
+				return deferred.promise;
 			}
 			else if (constraintName === "subject") {
-				return ["Math", "English", "Programming", "Science", "History"]
+				url = SUBJECT_URL;
+				//return ["Math", "English", "Programming", "Science", "History"]
 			}
 			else if (constraintName === "teacher") {
-				return ["Karyl Heider", "Ralph Winterspoon", "Leeroy Jenkins", "Mrs. Buttersworth"]
+				url = TEACHER_URL;
+				//return ["Karyl Heider", "Ralph Winterspoon", "Leeroy Jenkins", "Mrs. Buttersworth"]
 			}
 			else if (constraintName === "student") {
-				return ["Harry Potter", "Frodo Baggins", "Luke Skywalker", "Tron"]
+				url = STUDENT_URL;
+				//return ["Harry Potter", "Frodo Baggins", "Luke Skywalker", "Tron"]
 			}
 			else if (constraintName === "classroom") {
-				return ["1001", "1002", "203L", "West 123"]
+				url = CLASSROOM_URL;
+				//return ["1001", "1002", "203L", "West 123"]
 			}
 			else if (constraintName === "course" || constraintName === "required course" ) {
-				return ["English III"]
+				url = COURSE_URL;
+				//return ["English III"]
 			}
 			else if (constraintName === "student group") {
-				return ["4th grade", "5th grade", "6th grade", "student body"]
+				url = STUDENT_GROUP_URL;
+				//return ["4th grade", "5th grade", "6th grade", "student body"]
 			}
 			else {
 				console.error("tableEntriesService.getConstraintFacts: unexpected state: invalid constraintName: " + constraintName);
-				return null;
+				deferred.reject({data: "ERROR: Unexpected front-end input.", index: index});
+				return deferred.promise;
 			}
+
+			// conform the url to change the port
+			url = commonService.conformUrl(url);
+
+			$http.get(url).then(function(data) {
+				// @TODO: this should automatically be conformed later
+				
+				var conformedData = [], i;
+				// conform based on url, since we have no other way of knowing what type it is
+				if (data.config.url.indexOf("student_groups") > 0) {
+					conformedData = ["4th grade", "5th grade", "6th grade", "student body"];
+				}
+				else if (data.config.url.indexOf("students") > 0) {
+					for (i = 0; i < data.data.length; i++) {
+						conformedData.push(data.data[i].first_name + " " + data.data[i].last_name);
+					}
+				}
+				else if (data.config.url.indexOf("teachers") > 0) {
+					for (i = 0; i < data.data.length; i++) {
+						conformedData.push(data.data[i].first_name + " " + data.data[i].last_name);
+					}
+				}
+				else if (data.config.url.indexOf("classrooms") > 0) {
+					for (i = 0; i < data.data.length; i++) {
+						conformedData.push(data.data[i].room_number);
+					}
+				}
+				else if (data.config.url.indexOf("courses") > 0) {
+					for (i = 0; i < data.data.length; i++) {
+						conformedData.push(data.data[i].name);
+					}
+				}
+				else if (data.config.url.indexOf("subjects") > 0) {
+					// blank
+					conformedData = ["Math", "English", "Programming", "Science", "History"];
+				}
+				else {
+					console.error("tableEntriesService.getConstraintFacts: unexpected url reuturn: " + data.config.url);
+				}
+				// @NOTE: don't forget about index
+				deferred.resolve({data: conformedData, index: index});
+			}, function(data) {
+				deferred.reject({data: data.data, index: index});
+			});
+			return deferred.promise;
 		}
 	};
 }]);
