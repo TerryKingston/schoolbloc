83dd07f956d87cfdda8a09f971893681112b9790
bandre7771 <benjama48@hotmail.com>
2016-03-02 10:43:25 -0700

    Fixed timeblock and subjects api

--- project_implementation/flask_app/schoolbloc/scheduler/models.py
+++ project_implementation/flask_app/schoolbloc/scheduler/models.py
@@ -30,7 +30,7 @@ class Course(db.Model, SqlalchemySerializer):
     """
     __tablename__ = 'courses'
     __restconstraints__ = ['courses_student_groups', 'courses_students', 'courses_subjects', 'courses_timeblocks',
-                           'courses_teachers', 'classrooms_courses']
+                           'courses_teachers', 'classrooms_courses', 'scheduled_class']
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
     duration = db.Column(db.Integer) # optional duration, will use global default if not specified
@@ -52,8 +52,6 @@ class CoursesStudent(db.Model, SqlalchemySerializer):
     the Class object.
     """
     __tablename__ = 'courses_students'
-    __restconstraints__ = ['courses_student_groups', 'courses_subjects', 'courses_timeblocks',
-                           'courses_teachers', 'classrooms_courses']
     id = db.Column(db.Integer, primary_key=True)
     course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
     student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
@@ -77,8 +75,6 @@ class CoursesTeacher(db.Model, SqlalchemySerializer):
     the Class object.
     """
     __tablename__ = 'courses_teachers'
-    __restconstraints__ = ['courses_student_groups', 'courses_students', 'courses_subjects', 'courses_timeblocks',
-                           'classrooms_courses']
     id = db.Column(db.Integer, primary_key=True)
     course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
     teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'), nullable=False)
@@ -102,8 +98,6 @@ class CoursesStudentGroup(db.Model, SqlalchemySerializer):
     the Class object.
     """
     __tablename__ = 'courses_student_groups'
-    __restconstraints__ = ['courses_students', 'courses_subjects', 'courses_timeblocks',
-                           'courses_teachers', 'classrooms_courses']
     id = db.Column(db.Integer, primary_key=True)
     course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
     student_group_id = db.Column(db.Integer, db.ForeignKey('student_groups.id'), nullable=False)
@@ -127,8 +121,6 @@ class CoursesSubject(db.Model, SqlalchemySerializer):
     the Class object.
     """
     __tablename__ = 'courses_subjects'
-    __restconstraints__ = ['courses_student_groups', 'courses_students', 'courses_timeblocks',
-                           'courses_teachers', 'classrooms_courses']
     id = db.Column(db.Integer, primary_key=True)
     course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
     subject_id = db.Column(db.Integer, db.ForeignKey('subjects.id'), nullable=False)
@@ -150,7 +142,7 @@ class Classroom(db.Model, SqlalchemySerializer):
     """
     __tablename__ = 'classrooms'
     __restconstraints__ = ['classrooms_teachers', 'classrooms_courses', 'classrooms_timeblocks',
-                           'classrooms_subjects']
+                           'classrooms_subjects', 'scheduled_class']
     id = db.Column(db.Integer, primary_key=True)
     room_number = db.Column(db.Integer, nullable=False, unique=True)  # user assigned room number
 
@@ -161,8 +153,6 @@ class Classroom(db.Model, SqlalchemySerializer):
 class ClassroomsSubject(db.Model, SqlalchemySerializer):
 
     __tablename__ = 'classrooms_subjects'
-    __restconstraints__ = ['classrooms_teachers', 'classrooms_courses', 'classrooms_timeblocks']
-
     id = db.Column(db.Integer, primary_key=True)
     classroom_id = db.Column(db.Integer, db.ForeignKey('classrooms.id'), nullable=False)
     subject_id = db.Column(db.Integer, db.ForeignKey('subjects.id'), nullable=False)
@@ -186,7 +176,6 @@ class ClassroomsTeacher(db.Model, SqlalchemySerializer):
     the Class object.
     """
     __tablename__ = 'classrooms_teachers'
-    __restconstraints__ = ['classrooms_courses', 'classrooms_timeblocks', 'classrooms_subjects']
     id = db.Column(db.Integer, primary_key=True)
     classroom_id = db.Column(db.Integer, db.ForeignKey('classrooms.id'), nullable=False)
     teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'), nullable=False)
@@ -209,7 +198,6 @@ class ClassroomsCourse(db.Model, SqlalchemySerializer):
     The actual assignement of a classroom to a course happens in the Class object
     """
     __tablename__ = 'classrooms_courses'
-    __restconstraints__ = ['classrooms_teachers', 'classrooms_timeblocks', 'classrooms_subjects']
     id = db.Column(db.Integer, primary_key=True)
     classroom_id = db.Column(db.Integer, db.ForeignKey('classrooms.id'), nullable=False)
     course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
@@ -224,7 +212,6 @@ class ClassroomsCourse(db.Model, SqlalchemySerializer):
 
 class Schedule(db.Model, SqlalchemySerializer):
     __tablename__ = 'schedules'
-    __restconstraints__ = ['scheduled_classes_student']
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40), nullable=False)
     # backref scheduled_classes
@@ -352,8 +339,6 @@ class StudentsStudentGroup(db.Model, SqlalchemySerializer):
     the Class object.
     """
     __tablename__ = 'students_student_groups'
-    __restconstraints__ = ['students_timeblocks', 'students_subjects', 'courses_students', 'student_groups_timeblocks'
-                           'student_groups_subjects']
     id = db.Column(db.Integer, primary_key=True)
     student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
     student_group_id = db.Column(db.Integer, db.ForeignKey('student_groups.id'), nullable=False)
@@ -370,8 +355,6 @@ class StudentsSubject(db.Model, SqlalchemySerializer):
     ORM Object for linking table between students and subjects
     """
     __tablename__ = 'subjects_students'
-    __restconstraints__ = ['students_student_groups', 'students_timeblocks', 'students_subjects', 'courses_students',
-                           'student_groups_subjects', 'student_groups_timeblocks']
     id = db.Column(db.Integer, primary_key=True)
     student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
     subject_id = db.Column(db.Integer, db.ForeignKey('subjects.id'), nullable=False)
@@ -392,7 +375,7 @@ class Subject(db.Model, SqlalchemySerializer):
     """
     __tablename__ = 'subjects'
     __restconstraints__ = ['courses_subjects', 'student_groups_subjects', 'students_subjects', 'subjects_timeblocks',
-                           'teachers_subjects', 'teachers_subjects', 'subjects_timeblocks']
+                           'teachers_subjects', 'teachers_subjects', 'subjects_timeblocks', 'classrooms_subjects']
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
 
@@ -409,7 +392,7 @@ class Teacher(db.Model, SqlalchemySerializer):
     """
     __tablename__ = 'teachers'
     __restconstraints__ = ['classrooms_teachers', 'teachers_timeblocks', 'courses_teachers',
-                           'teachers_subjects']
+                           'teachers_subjects', 'scheduled_class']
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(128), nullable=False)
     last_name = db.Column(db.String(128), nullable=False)
@@ -424,7 +407,6 @@ class Teacher(db.Model, SqlalchemySerializer):
 
 class TeachersSubject(db.Model, SqlalchemySerializer):
     __tablename__ = 'teachers_subjects'
-    __restconstraints__ = ['classrooms_teachers', 'teachers_timeblocks', 'courses_teachers']
     id = db.Column(db.Integer, primary_key=True)
     teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'), nullable=False)
     subject_id = db.Column(db.Integer, db.ForeignKey('subjects.id'), nullable=False)
@@ -442,8 +424,6 @@ class CoursesTimeblock(db.Model, SqlalchemySerializer):
     ORM Object for linking table between courses and timeblocks
     """
     __tablename__= 'courses_timeblocks'
-    __restconstraints__ = ['courses_student_groups', 'courses_students', 'courses_subjects',
-                           'courses_teachers', 'classrooms_courses']
     id = db.Column(db.Integer, primary_key=True)
     course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
     timeblock_id = db.Column(db.Integer, db.ForeignKey('timeblocks.id'), nullable=False)
@@ -461,8 +441,6 @@ class ClassroomsTimeblock(db.Model, SqlalchemySerializer):
     ORM Object for linking table between classrooms and timeblocks
     """
     __tablename__= 'classrooms_timeblocks'
-    __restconstraints__ = ['classrooms_teachers', 'classrooms_courses',
-                           'classrooms_subjects']
     id = db.Column(db.Integer, primary_key=True)
     classroom_id = db.Column(db.Integer, db.ForeignKey('classrooms.id'), nullable=False)
     timeblock_id = db.Column(db.Integer, db.ForeignKey('timeblocks.id'), nullable=False)
@@ -480,8 +458,6 @@ class StudentGroupsTimeblock(db.Model, SqlalchemySerializer):
     ORM Object for linking table between classrooms and timeblocks
     """
     __tablename__= 'student_groups_timeblocks'
-    __restconstraints__ = ['students_student_groups', 'students_timeblocks', 'students_subjects', 'courses_students',
-                           'student_groups_subjects']
     id = db.Column(db.Integer, primary_key=True)
     student_group_id = db.Column(db.Integer, db.ForeignKey('student_groups.id'), nullable=False)
     timeblock_id = db.Column(db.Integer, db.ForeignKey('timeblocks.id'), nullable=False)
@@ -499,8 +475,6 @@ class StudentsTimeblock(db.Model, SqlalchemySerializer):
     ORM Object for linking table between classrooms and timeblocks
     """
     __tablename__= 'students_timeblocks'
-    __restconstraints__ = ['students_student_groups', 'students_subjects', 'courses_students',
-                           'student_groups_timeblocks', 'student_groups_subjects']
     id = db.Column(db.Integer, primary_key=True)
     student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
     timeblock_id = db.Column(db.Integer, db.ForeignKey('timeblocks.id'), nullable=False)
@@ -518,8 +492,6 @@ class SubjectsTimeblock(db.Model, SqlalchemySerializer):
     ORM Object for linking table between subjects and timeblocks
     """
     __tablename__= 'subjects_timeblocks'
-    __restconstraints__ = ['courses_subjects', 'student_groups_subjects', 'students_subjects', 'subjects_timeblocks',
-                           'teachers_subjects', 'teachers_subjects']
     id = db.Column(db.Integer, primary_key=True)
     subject_id = db.Column(db.Integer, db.ForeignKey('subjects.id'), nullable=False)
     timeblock_id = db.Column(db.Integer, db.ForeignKey('timeblocks.id'), nullable=False)
@@ -536,8 +508,6 @@ class TeachersTimeblock(db.Model, SqlalchemySerializer):
     ORM Object for linking table between teachers and timeblocks
     """
     __tablename__= 'teachers_timeblocks'
-    __restconstraints__ = ['classrooms_teachers', 'courses_teachers',
-                           'teachers_subjects']
     id = db.Column(db.Integer, primary_key=True)
     teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'), nullable=False)
     timeblock_id = db.Column(db.Integer, db.ForeignKey('timeblocks.id'), nullable=False)
@@ -560,7 +530,7 @@ class Timeblock(db.Model, SqlalchemySerializer):
     """
     __tablename__ = 'timeblocks'
     __restconstraints__ = ['classrooms_timeblocks', 'courses_timeblocks', 'students_timeblocks',
-                           'student_groups_timeblocks', 'teachers_timeblocks']
+                           'student_groups_timeblocks', 'teachers_timeblocks', 'subjects_timeblocks']
     id = db.Column(db.Integer, primary_key=True)
     start_time = db.Column(db.Integer, nullable=False)
     end_time = db.Column(db.Integer, nullable=False)
